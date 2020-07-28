8814cf9c5995004771e0999e871ab34ac826a758
yuanasdfg <yuanasdfgh@gmail.com>
2020-07-28 11:09:51 +0800

    涛小普

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,8 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+
 })
 
 export default router
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,230 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <li>Lorem ipsum dolor sit.</li>
+    <li>Id autem ipsum minima!</li>
+    <li>Eveniet obcaecati reprehenderit similique.</li>
+    <li>Reiciendis culpa deleniti sunt?</li>
+    <li>Nobis voluptas nostrum veritatis!</li>
+    <li>Natus, iste? Atque, sed.</li>
+    <li>Qui fuga id quam!</li>
+    <li>Perspiciatis quo sed reprehenderit?</li>
+    <li>Quaerat excepturi quasi dolore?</li>
+    <li>Rem dignissimos possimus illo.</li>
+    <li>Eos distinctio laboriosam non?</li>
+    <li>Repellat pariatur ad impedit.</li>
+    <li>Voluptas incidunt id sunt!</li>
+    <li>Dolore sed maiores facilis.</li>
+    <li>Eligendi quos accusamus corporis!</li>
+    <li>Quisquam doloremque labore illo.</li>
+    <li>Vitae aliquam eligendi cumque.</li>
+    <li>Iure asperiores saepe mollitia!</li>
+    <li>Ullam quisquam corporis mollitia.</li>
+    <li>Neque, fugit quo? Voluptates.</li>
+    <li>Dolore tempore eaque neque?</li>
+    <li>Autem officia iusto unde!</li>
+    <li>Nemo, distinctio consectetur? Quos.</li>
+    <li>Placeat recusandae non sequi!</li>
+    <li>Facilis aspernatur expedita nostrum.</li>
+    <li>Dolores ullam repellat voluptatibus.</li>
+    <li>Non ullam nostrum blanditiis!</li>
+    <li>Esse dignissimos animi obcaecati.</li>
+    <li>Nesciunt magnam commodi fugiat!</li>
+    <li>Fuga asperiores unde quisquam.</li>
+    <li>Libero fugit quas dolor.</li>
+    <li>Distinctio ratione eum labore.</li>
+    <li>Magni culpa cum quia.</li>
+    <li>Dolorum rem vero vel?</li>
+    <li>Sapiente magnam modi consectetur.</li>
+    <li>Corporis ut illo neque!</li>
+    <li>Ullam eligendi mollitia voluptates.</li>
+    <li>Asperiores architecto perferendis numquam?</li>
+    <li>Possimus ea facilis reiciendis.</li>
+    <li>Quisquam a soluta nobis.</li>
+    <li>Mollitia praesentium illo eum!</li>
+    <li>Ab sit corrupti dolore.</li>
+    <li>Cupiditate quis iusto eligendi.</li>
+    <li>Fuga laudantium non quae?</li>
+    <li>Recusandae quam voluptas voluptatibus.</li>
+    <li>Mollitia molestiae sed quibusdam.</li>
+    <li>Vero tenetur exercitationem quidem.</li>
+    <li>Et commodi ex nemo!</li>
+    <li>Iure nulla officia nostrum.</li>
+    <li>Doloremque, totam. Odio, aut!</li>
+    <li>Laudantium accusantium architecto repellat.</li>
+    <li>Cum voluptatem error quo.</li>
+    <li>Minus doloremque ab dolore.</li>
+    <li>Molestias sunt velit a!</li>
+    <li>Nihil aspernatur quaerat at?</li>
+    <li>Voluptates maxime modi ipsum?</li>
+    <li>Est nisi commodi obcaecati.</li>
+    <li>Nulla vel atque laborum!</li>
+    <li>Magni officia omnis aperiam.</li>
+    <li>Ducimus minus omnis molestias.</li>
+    <li>Eos at maxime tempore!</li>
+    <li>Illo eum quos repellendus!</li>
+    <li>Odit soluta fugit facere.</li>
+    <li>Inventore beatae deleniti optio.</li>
+    <li>Dolorem exercitationem voluptas placeat.</li>
+    <li>Aspernatur quo distinctio omnis?</li>
+    <li>Optio laboriosam veritatis odio.</li>
+    <li>Enim placeat libero itaque.</li>
+    <li>Repellat cum pariatur dolorum.</li>
+    <li>Unde ab a quam.</li>
+    <li>Soluta quibusdam voluptatum deleniti!</li>
+    <li>Soluta cumque eum nesciunt?</li>
+    <li>Consequuntur, quo! Temporibus, eveniet.</li>
+    <li>Libero animi delectus perferendis?</li>
+    <li>Quod minus adipisci nesciunt.</li>
+    <li>Accusantium hic tenetur optio.</li>
+    <li>Eius libero quis eaque!</li>
+    <li>Aspernatur commodi reiciendis ipsam!</li>
+    <li>Excepturi aspernatur quibusdam unde!</li>
+    <li>Ipsa atque laudantium laboriosam.</li>
+    <li>Cumque deserunt debitis ab?</li>
+    <li>Nobis ullam vel in.</li>
+    <li>Tenetur quidem mollitia culpa.</li>
+    <li>Nulla mollitia tempore labore.</li>
+    <li>Beatae voluptates culpa provident.</li>
+    <li>Maiores voluptatibus alias dignissimos!</li>
+    <li>Illum quo quidem quod?</li>
+    <li>Quidem perferendis totam voluptatem.</li>
+    <li>Maiores nisi quisquam vitae.</li>
+    <li>Ipsa, maiores rem. Et.</li>
+    <li>Ipsa esse voluptate possimus.</li>
+    <li>Illum rerum modi voluptate!</li>
+    <li>Sunt ullam maxime molestias.</li>
+    <li>Eaque quae culpa vero.</li>
+    <li>Molestias doloribus consequuntur minus.</li>
+    <li>Quis dicta eum numquam!</li>
+    <li>Dignissimos harum recusandae officia.</li>
+    <li>Officiis blanditiis ducimus provident?</li>
+    <li>Alias dicta laudantium consequuntur.</li>
+    <li>Necessitatibus incidunt quas repellendus?</li>
+    <li>Lorem, ipsum dolor.</li>
+    <li>Quas, nulla veritatis.</li>
+    <li>Eos, tempora modi.</li>
+    <li>Ea, magnam eaque.</li>
+    <li>Reiciendis, similique esse!</li>
+    <li>Similique, doloribus ad.</li>
+    <li>Repellendus, deserunt tempore?</li>
+    <li>Fuga, libero voluptates!</li>
+    <li>Odit, ducimus facere.</li>
+    <li>Sapiente, possimus. Voluptatem!</li>
+    <li>Voluptate, eligendi magni!</li>
+    <li>Error, molestias suscipit.</li>
+    <li>Iusto, illo officiis!</li>
+    <li>Molestias, accusamus eum?</li>
+    <li>Dignissimos, porro sequi.</li>
+    <li>Voluptatum, natus debitis.</li>
+    <li>Nihil, aliquid excepturi.</li>
+    <li>Facilis, eum? Ad!</li>
+    <li>Repellendus, saepe temporibus!</li>
+    <li>Dolor, numquam saepe.</li>
+    <li>Consequatur, vitae obcaecati!</li>
+    <li>At, modi tempora?</li>
+    <li>Recusandae, tenetur consequuntur.</li>
+    <li>Quos, est tempora?</li>
+    <li>Laborum, nam animi!</li>
+    <li>Beatae, quidem. Provident!</li>
+    <li>Sapiente, corrupti quibusdam.</li>
+    <li>Ipsum, eligendi consequatur.</li>
+    <li>Similique, quo ab.</li>
+    <li>Soluta, dignissimos sequi.</li>
+    <li>Vitae, iure laboriosam?</li>
+    <li>Maxime, natus rem!</li>
+    <li>Ea, sint accusantium?</li>
+    <li>Delectus, inventore ut.</li>
+    <li>Nobis, qui minima!</li>
+    <li>Necessitatibus, quisquam unde!</li>
+    <li>Deserunt, inventore impedit?</li>
+    <li>Vel, voluptates itaque.</li>
+    <li>Eligendi, voluptas quaerat.</li>
+    <li>Ipsum, magni at.</li>
+    <li>Provident, deserunt repellendus.</li>
+    <li>Provident, quam officia.</li>
+    <li>Quam, architecto! Quo?</li>
+    <li>Quas, pariatur aliquid!</li>
+    <li>Eligendi, odio aspernatur?</li>
+    <li>Commodi, recusandae nobis?</li>
+    <li>Officia, corrupti voluptatibus.</li>
+    <li>Repellat, beatae at?</li>
+    <li>Vitae, voluptates impedit.</li>
+    <li>Expedita, deleniti! Similique.</li>
+    <li>Repudiandae, consectetur quisquam!</li>
+    <li>Velit, in impedit.</li>
+    <li>Omnis, cupiditate dolores.</li>
+    <li>Veniam, dolorem fuga.</li>
+    <li>Nam, natus id.</li>
+    <li>Expedita, dignissimos at.</li>
+    <li>Quidem, ea iste.</li>
+    <li>Aliquam, magni eos?</li>
+    <li>Optio, iure numquam.</li>
+    <li>Dolor, reiciendis facere.</li>
+    <li>Animi, repudiandae dicta?</li>
+    <li>Molestias, quis fugit.</li>
+    <li>Reiciendis, numquam dolorem.</li>
+    <li>Possimus, beatae aliquam.</li>
+    <li>Perspiciatis, error voluptatem?</li>
+    <li>Minima, dolorum debitis!</li>
+    <li>Impedit, doloremque sapiente?</li>
+    <li>Ipsum, repellat provident.</li>
+    <li>Repudiandae, modi nam.</li>
+    <li>Earum, ullam ipsa?</li>
+    <li>Eum, praesentium optio.</li>
+    <li>Sint, amet sequi!</li>
+    <li>Cupiditate, mollitia et?</li>
+    <li>Modi, quo ad.</li>
+    <li>Fugit, atque tempora.</li>
+    <li>Pariatur, inventore error.</li>
+    <li>Necessitatibus, ipsum nostrum?</li>
+    <li>Ad, facilis libero.</li>
+    <li>Ratione, vero illo?</li>
+    <li>Eaque, itaque perspiciatis.</li>
+    <li>Facilis, quos ipsa!</li>
+    <li>Adipisci, numquam aperiam.</li>
+    <li>Praesentium, eveniet odit?</li>
+    <li>Sit, laboriosam iure.</li>
+    <li>Iure, eaque rem.</li>
+    <li>Recusandae, aliquid quibusdam.</li>
+    <li>At, rem debitis?</li>
+    <li>Eaque, fugit eum.</li>
+    <li>Vero, tempore maxime.</li>
+    <li>Ex, eius quia.</li>
+    <li>Voluptas, vero necessitatibus!</li>
+    <li>Commodi, debitis hic.</li>
+    <li>Aliquid, doloribus magnam!</li>
+    <li>Suscipit, odit rem.</li>
+    <li>Laudantium, autem suscipit.</li>
+    <li>Sed, tempora ut.</li>
+    <li>Dolor, nam ducimus.</li>
+    <li>Pariatur, ad ducimus.</li>
+    <li>Quia, mollitia. Sed.</li>
+    <li>Nemo, explicabo quidem.</li>
   </div>
 </template>
+
+<script>
+export default {
+  
+ mounted() {
+         
+           
+                // let _this=this
+                window.addEventListener('scroll', () => {
+                    let top = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset
+                    console.log(top);
+                    if (top > 700) {
+                        this.scrollTag = true;
+                    } else {
+                        this.scrollTag = false
+                    }
+
+                })
+
+        }
+
+
+
+}
+</script>
